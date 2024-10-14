240e2f28cf516b3b008e6c9892368c4030bac97a
NittaMasayo <2922mpay@gmail>
2024-10-14 17:31:59 +0900

    TOP画面のWidgetテスト修正

---
+++ test/widget_test.dart
@@ -0,0 +1,6 @@
+import 'widget_test/top/top_widget.dart';
+
+void main() {
+  final topPage = TopWidget();
+  topPage.main();
+}
--- test/widget_test/top/top_widget.dart
+++ test/widget_test/top/top_widget.dart
@@ -2,20 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:memory_game/main.dart';
 
-void main() {
-  testWidgets('top page', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MemoryApp());
-    expect(find.text('チュートリアル'), findsOneWidget);
+class TopWidget {
+  void main() {
+    testWidgets('top page', (WidgetTester tester) async {
+      // Build our app and trigger a frame.
+      await tester.pumpWidget(const MemoryApp());
+      expect(find.text('チュートリアル'), findsOneWidget);
 
-    // Verify that our counter starts at 0.
-    expect(find.text('始める'), findsOneWidget);
-    final startButton = find.text("始める");
-    await tester.tap(startButton);
+      // Verify that our counter starts at 0.
+      expect(find.text('始める'), findsOneWidget);
+      final startButton = find.text("始める");
+      await tester.tap(startButton);
 
-    await tester.pumpAndSettle();
+      await tester.pumpAndSettle();
 
-    final gridItemsFinder = find.byKey(const ValueKey("tapGrid"));
-    expect(gridItemsFinder, findsOneWidget);
-  });
+      final gridItemsFinder = find.byKey(const ValueKey("tapGrid"));
+      expect(gridItemsFinder, findsOneWidget);
+    });
+  }
 }
