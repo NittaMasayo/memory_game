b75274346bd5f36d1f082b9e9699da7aa6be8782
NittaMasayo <2922mpay@gmail>
2024-10-20 15:48:55 +0900

    スコア保存時の挙動修正

--- lib/presentation/view_model/score_preference.dart
+++ lib/presentation/view_model/score_preference.dart
@@ -1,3 +1,4 @@
+import 'package:mneme/business/repository/base_repository.dart';
 import 'package:mneme/business/repository/shared_repository.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
@@ -6,20 +7,25 @@ part 'score_preference.g.dart';
 
 @riverpod
 class ScorePreference extends _$ScorePreference {
-  late SharedRepository repository;
+  BaseRepository? repository;
   Future<int> _initScore() async {
     final pref = await SharedPreferences.getInstance();
     const scoreKeyName = "score";
     repository = SharedRepository(pref, scoreKeyName);
-    return repository.getScore();
+    if (repository != null) {
+      return repository!.getScore();
+    }
+    return 0;
   }
 
   @override
-  Future<void> build() {
+  Future<int> build() {
     return _initScore();
   }
 
-  Future<void> setNewScore(int newScore) {
-    return repository.setScore(newScore);
+  Future<void> setNewScore(int newScore) async {
+    if (repository != null) {
+      repository!.setScore(newScore);
+    }
   }
 }
--- lib/presentation/view_model/score_preference.g.dart
+++ lib/presentation/view_model/score_preference.g.dart
@@ -6,12 +6,12 @@ part of 'score_preference.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$scorePreferenceHash() => r'e7b750a2b4f946a5abd8c10ab6f9b9bf1ec3c7b1';
+String _$scorePreferenceHash() => r'a1714add2632b0493db9826bdb70531126efdc9e';
 
 /// See also [ScorePreference].
 @ProviderFor(ScorePreference)
 final scorePreferenceProvider =
-    AutoDisposeAsyncNotifierProvider<ScorePreference, void>.internal(
+    AutoDisposeAsyncNotifierProvider<ScorePreference, int>.internal(
   ScorePreference.new,
   name: r'scorePreferenceProvider',
   debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
@@ -21,6 +21,6 @@ final scorePreferenceProvider =
   allTransitiveDependencies: null,
 );
 
-typedef _$ScorePreference = AutoDisposeAsyncNotifier<void>;
+typedef _$ScorePreference = AutoDisposeAsyncNotifier<int>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
